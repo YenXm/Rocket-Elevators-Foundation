b861b41ab371e8a76babf86c650b06fac2bfb64a
YenXm <moonvoyager2@protonmail.com>
2021-11-30 21:25:37 -0500

    Zendesk

--- app/admin/interventions.rb
+++ app/admin/interventions.rb
@@ -55,9 +55,42 @@ ActiveAdmin.register Intervention do
         end
         inputs do
             input :employee_id, as: :select, collection: Employee.all
-            input :report, label: 'Description'
+            input :report, label: 'Description', required: true
         end
 
         f.actions # adds the 'Submit' and 'Cancel' buttons
+    end
+
+    controller do
+        # Modify the create method that come with inherited_resources gem
+        # POST /resources
+        def create(options = {}, &block)
+            object = build_resource
+
+            options[:location] ||= smart_resource_url if create_resource(object)
+
+            respond_with_dual_blocks(object, options, &block)
+
+            ZendeskAPI::Ticket.create!(
+                @client,
+                subject: 'New Intervention',
+                requester: {
+                    'name': object.author.email,
+                },
+                description: object.report,
+                # Make sure the you select the good form in zendesk to see these field
+                custom_fields: [
+                    { id: 360_049_401_091, value: object.customer },
+                    { id: 1_900_003_022_547, value: object.building_id },
+                    { id: 360_049_401_211, value: object.battery_id },
+                    { id: 360_049_337_492, value: object.column_id || '' },
+                    { id: 1_900_003_022_487, value: object.elevator_id || '' },
+                    { id: 360_049_401_231, value: object.employee },
+                ],
+                type: :incident,
+                priority: :normal,
+            )
+        end
+        alias create! create
     end
 end
