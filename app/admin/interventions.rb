099a99e2cd3d49b3f01e4a00023644c9eab27124
YenXm <moonvoyager2@protonmail.com>
2021-12-03 12:56:35 -0500

    Fixed zendesk

--- app/admin/interventions.rb
+++ app/admin/interventions.rb
@@ -20,35 +20,28 @@ ActiveAdmin.register Intervention do
     form do |f|
         f.semantic_errors # shows errors on :base
         inputs do
-            input :author_id,
-                  as: :select,
-                  collection: [current_user.employees.first],
-                  include_blank: false
+            input :author_id, as: :select, collection: [current_user.employees.first], include_blank: false
             input :customer_id,
                   as: :select,
                   prompt: 'Select customer',
                   collection: Customer.all,
                   input_html: {
-                      onchange:
-                          'cascade("customer","building"); collection("customer", "building")',
+                      onchange: 'cascade("customer","building"); collection("customer", "building")',
                   }
             input :building_id,
                   as: :select,
                   input_html: {
-                      onchange:
-                          'collection("building", "battery"); cascade("building","battery")',
+                      onchange: 'collection("building", "battery"); cascade("building","battery")',
                   }
             input :battery_id,
                   as: :select,
                   input_html: {
-                      onchange:
-                          'cascade("battery","column"); collection("battery", "column")',
+                      onchange: 'cascade("battery","column"); collection("battery", "column")',
                   }
             input :column_id,
                   as: :select,
                   input_html: {
-                      onchange:
-                          'cascade("column","elevator"); collection("column", "elevator")',
+                      onchange: 'cascade("column","elevator"); collection("column", "elevator")',
                   }
 
             input :elevator_id, as: :select
@@ -80,12 +73,12 @@ ActiveAdmin.register Intervention do
                 description: object.report,
                 # Make sure the you select the good form in zendesk to see these field
                 custom_fields: [
-                    { id: 360_049_401_091, value: object.customer },
-                    { id: 1_900_003_022_547, value: object.building_id },
-                    { id: 360_049_401_211, value: object.battery_id },
-                    { id: 360_049_337_492, value: object.column_id || '' },
-                    { id: 1_900_003_022_487, value: object.elevator_id || '' },
-                    { id: 360_049_401_231, value: object.employee },
+                    { id: 1_900_005_996_565, value: object.customer },
+                    { id: 1_500_013_299_021, value: object.building_id },
+                    { id: 1_900_005_996_625, value: object.battery_id },
+                    { id: 1_900_005_996_665, value: object.column_id || '' },
+                    { id: 1_500_013_381_822, value: object.elevator_id || '' },
+                    { id: 1_500_013_381_842, value: object.employee },
                 ],
                 type: :incident,
                 priority: :normal,
