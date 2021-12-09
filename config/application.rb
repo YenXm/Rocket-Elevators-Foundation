f3d4fa6c5652e17b93c87d43893989577d66c6d5
YenXm <moonvoyager2@protonmail.com>
2021-12-09 18:55:06 -0500

    a

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ Dotenv::Railtie.load
 module App
     class Application < Rails::Application
         # Initialize configuration defaults for originally generated Rails version.
-        config.load_defaults 5.2
+        # config.load_defaults 5.2
+        
 
         # Settings in config/environments/* take precedence over those specified here.
         # Application configuration can go into files in config/initializers
